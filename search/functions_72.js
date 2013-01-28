fb7c6b967ddbdddc08e3271accd09dfd0744261c
David Wilson <dw@botanicus.net>
2013-01-28 17:43:24 +0200

    Update documentation

--- search/functions_72.js
+++ search/functions_72.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['remove',['remove',['../classetree_1_1_element.html#afcca584486ba7becdf9b2a499b557877',1,'etree::Element']]]
+  ['remove',['remove',['../classetree_1_1_element.html#afcca584486ba7becdf9b2a499b557877',1,'etree::Element::remove(Element &amp;e)'],['../classetree_1_1_element.html#a6da5e137f1ad1c2122fd057486f5f6b7',1,'etree::Element::remove()']]]
 ];
