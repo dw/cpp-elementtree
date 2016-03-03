c22d7c6b72ac900f0b2f6e904bb5e44f3ffd29e7
David Wilson <dw@botanicus.net>
2016-03-03 01:39:29 +0000

    Update documentation

--- search/functions_4.js
+++ search/functions_4.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['element',['Element',['../classetree_1_1_element.html#acd99a348e3b45aaa0ae0aa846819264e',1,'etree::Element::Element(const Element &amp;e)'],['../classetree_1_1_element.html#ac6985ee5f7314991360c3099d4ab3940',1,'etree::Element::Element(const QName &amp;qname)'],['../classetree_1_1_element.html#a1b4188191b464973c932fff484315235',1,'etree::Element::Element(const QName &amp;qname, kv_list attribs)'],['../classetree_1_1feed_1_1_feed.html#a54eb1505bc74955654d782fd3915c4c5',1,'etree::feed::Feed::element()']]],
   ['end',['end',['../classetree_1_1_attr_map.html#a14237af86d41bd1e7507e60c7bf438e3',1,'etree::AttrMap::end()'],['../classetree_1_1_element.html#a7dc3605cc6af61c0f62e57e8e9137851',1,'etree::Element::end()']]],
+  ['ensurechild',['ensurechild',['../classetree_1_1_element.html#a8fa7f7760d3a29f95e6e941216589c9c',1,'etree::Element']]],
   ['ensurens',['ensurens',['../classetree_1_1_element.html#aaefe149333c54d77f8fb4969965a0772',1,'etree::Element']]],
   ['equals',['equals',['../classetree_1_1_q_name.html#a548b29b2f3892461e6388c19348ec588',1,'etree::QName']]],
   ['expr',['expr',['../classetree_1_1_x_path.html#afe3004d3ac36030f77baff88d293accb',1,'etree::XPath']]]
